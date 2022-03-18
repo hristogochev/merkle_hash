52634b70b544a809f821b40aaf130d4a7e3c257f
Scotty <hristogochev35@gmail.com>
2022-03-18 23:51:59 +0200

    Removed unnecessary tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 Crate `merkle_hash` makes it easy to find the Blake3 hash
 of files or entire directories using a Multithreaded Merkle tree algorithm
 
+# Usage
 
 To use this crate, add `merkle_hash` as a dependency to your project's
 `Cargo.toml`
@@ -62,48 +63,3 @@ pub mod merkle_utils;
 
 pub use crate::merkle_item::MerkleItem;
 pub use crate::merkle_utils::{find_merkle_hash, get_hashes, get_paths};
-
-#[cfg(test)]
-mod tests {
-    use crate::merkle_item::MerkleItem;
-    use crate::merkle_utils::{find_merkle_hash, get_hashes, get_paths};
-    use blake3::{hash, Hash};
-    use std::path::{Path, PathBuf};
-    use std::str::FromStr;
-
-    #[test]
-    fn test_find_merkle_hash() {
-        let first_hash = hash(b"foo");
-        let second_hash = hash(b"bar");
-        let third_hash = hash(b"baz");
-        let fourth_hash = hash(b"cow");
-        let hashes = vec![first_hash, second_hash, third_hash, fourth_hash];
-
-        let merkle_hash = find_merkle_hash(&hashes);
-
-        let expected_hash_output =
-            Hash::from_str("d804c32ea1170b63ba7ede0d1e32ca75640541a59360a183cc4462cedb571b36").ok();
-
-        assert_eq!(merkle_hash, expected_hash_output);
-    }
-
-    #[test]
-    fn test_hash_paths() {
-        let root = Path::new("/root/to/get/paths/from");
-        let paths = get_paths(root);
-        let _hashes = get_hashes(&paths);
-    }
-
-    #[test]
-    fn test_get_paths() {
-        let root = Path::new("/root/to/get/paths/from");
-        let _paths = get_paths(root);
-    }
-
-    #[test]
-    fn test_merkle_item() {
-        let path = PathBuf::from("/root/to/get/paths/from");
-        let merkle_item = MerkleItem::new(path);
-        let _single_hash = merkle_item.get_hash();
-    }
-}
